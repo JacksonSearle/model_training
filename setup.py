cf9134881382dfc40e2565e02b24fd83f5c5a6f8
Jackson Searle <jacksonsearle24@gmail.com>
2023-09-26 14:29:11 -0600

    Fixes text files

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
 from transformers import AutoTokenizer
-
-os.system('huggingface-cli login --token $HF_TOKEN')  # Ensure you have either [A] exported an env var w/ the corresponding token (recommended) or [B] replaced '$HF_TOKEN' w/ your exact token
+from datasets import books
 
 # def download_llama():
 #     print(f'\n\nDownloading Llama\r')
@@ -11,19 +10,14 @@ os.system('huggingface-cli login --token $HF_TOKEN')  # Ensure you have either [
 
 def download_dataset():
     #TODO: Download books to dataset folder
-    books = {
-        "The Book of Mormon": "https://www.gutenberg.org/files/17/17-0.txt",
-        "The Letters of Jane Austen": "https://www.gutenberg.org/cache/epub/42078/pg42078.txt",
-        "My Bondage and My Freedom": "https://www.gutenberg.org/cache/epub/202/pg202.txt",
-        "Anne of Green Gables": "https://www.gutenberg.org/cache/epub/45/pg45.txt",
-        "Romeo and Juliet": "https://www.gutenberg.org/cache/epub/1513/pg1513.txt"
-    }
     print(f'\n\nDownloading datasets')
     directory_name = "datasets"
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
 
     for name, link in books.items():
+        name = f'{directory_name}/{name}.txt'
+        print(name)
         if not os.path.exists(name):
             try:
                 subprocess.check_call([
