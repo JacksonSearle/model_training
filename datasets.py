e8e9a657fe0a9bb8857f824a8a4c349e4a5dcba1
Jackson Searle <jacksonsearle24@gmail.com>
2023-09-26 14:45:14 -0600

    books variable back in setup.py

--- datasets.py
+++
@@ -1,7 +0,0 @@
-books = {
-    "The Book of Mormon": "https://www.gutenberg.org/files/17/17-0.txt",
-    "The Letters of Jane Austen": "https://www.gutenberg.org/cache/epub/42078/pg42078.txt",
-    "My Bondage and My Freedom": "https://www.gutenberg.org/cache/epub/202/pg202.txt",
-    "Anne of Green Gables": "https://www.gutenberg.org/cache/epub/45/pg45.txt",
-    "Romeo and Juliet": "https://www.gutenberg.org/cache/epub/1513/pg1513.txt"
-}
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 from transformers import AutoTokenizer
-from datasets import books
 
 # def download_llama():
 #     print(f'\n\nDownloading Llama\r')
@@ -14,6 +13,14 @@ def download_dataset():
     directory_name = "datasets"
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
+    
+    books = {
+        "The Book of Mormon": "https://www.gutenberg.org/files/17/17-0.txt",
+        "The Letters of Jane Austen": "https://www.gutenberg.org/cache/epub/42078/pg42078.txt",
+        "My Bondage and My Freedom": "https://www.gutenberg.org/cache/epub/202/pg202.txt",
+        "Anne of Green Gables": "https://www.gutenberg.org/cache/epub/45/pg45.txt",
+        "Romeo and Juliet": "https://www.gutenberg.org/cache/epub/1513/pg1513.txt"
+    }
 
     for name, link in books.items():
         name = f'{directory_name}/{name}.txt'
