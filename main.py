b7e050cf5fb9875f9466360eb8f40a5f74751946
Jackson Searle <jacksonsearle24@gmail.com>
2023-09-26 20:03:49 -0600

    It works now, isn't that cool?

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
 import os
+import datasets
 from datasets import load_dataset
-from transformers import DataCollatorWithPadding
+from transformers import DataCollatorForLanguageModeling
 from transformers import TrainingArguments, Trainer
 from model import model, tokenizer
-
-def load_datasets(data_files):
-    dataset = load_dataset('text', data_files=data_files)
-    return dataset
-
 
 def tokenize_dataset(dataset):
     def tokenize_function(examples):
         output = tokenizer(
-            examples['text'],
-            max_length=512, #
-            truncation=True #
+            examples['text']
             )
-        # output["labels"] = output["input_ids"].clone()
         return output
-    
-    tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=dataset['train'].column_names)
-    # input_ids_column = tokenized_datasets['input_ids']
-    # tokenized_datasets.add_column(name='labels', column=input_ids_column)
-    # tokenized_datasets = tokenized_datasets.rename_column('input_ids', 'labels')
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
     return tokenized_datasets
 
+def get_dataset(dataset_path, text_file_path):
+    tokenized_path = f'{dataset_path}/tokenized'
+
+    if not os.path.exists(tokenized_path):
+        data_files = {'train': text_file_path}
+        train_dataset = load_dataset('text', data_files=data_files, sample_by="paragraph")
+        train_dataset = tokenize_dataset(train_dataset['train'])
+        train_dataset.save_to_disk(tokenized_path)
+    else:
+        train_dataset = datasets.load_from_disk(tokenized_path)
+    return train_dataset
 
 
-def train_model(train_dataset, data_collator, training_args, save_path):
+def main():
+    book_name = 'Romeo_and_Juliet'
+    dataset_path = f'datasets/{book_name}'
+    text_file_path = f'{dataset_path}/book.txt'
+
+    model_name = 'my_cool_model'
+    save_path = f'models/{model_name}'
+
+    train_dataset = get_dataset(dataset_path, text_file_path)
+    
+    training_args = TrainingArguments(
+        output_dir="models/my_cool_model",
+        learning_rate=1e-3,
+        num_train_epochs=1,
+        per_device_train_batch_size=1,
+        warmup_steps=500,
+    )
+    
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -38,30 +57,8 @@ def train_model(train_dataset, data_collator, training_args, save_path):
     trainer.train()
     trainer.save_model(save_path)
 
-
-def main():
-    dataset_path = "datasets/Romeo_and_Juliet/book.txt"
-    save_path = "models/my_cool_model/final_model"
-    tokenized_path = "tokenized"
-
-    train_dataset = load_datasets({'train': dataset_path})
-    train_dataset = tokenize_dataset(train_dataset['train'])
-    train_dataset.save_to_disk(tokenized_path)
-    
-    training_args = TrainingArguments(
-        output_dir="models/my_cool_model",
-        learning_rate=1e-3,
-        num_train_epochs=1,
-        per_device_train_batch_size=1,
-        warmup_steps=500,
-    )
-    
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, max_length=1024)
-    
-    train_model(train_dataset, data_collator, training_args, save_path)
-
-    prompt = "Once upon a time "
-    encoded = tokenizer(prompt, return_tensors="pt")
+    prompt = "But soft, what light through yonder window breaks?\n"
+    encoded = tokenizer(prompt, return_tensors="pt").to('cuda')
 
     output_answer = model.generate(encoded['input_ids'], max_length=200)
     decoded_answer = tokenizer.decode(output_answer[0])
